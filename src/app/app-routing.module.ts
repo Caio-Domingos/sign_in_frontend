c75b6e6308ef0500e8f596f98548b042b35768c8
token pitagoras <caiod.dev@gmail.com>
2019-05-30 22:03:55 -0300

    login form incomplete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: LoginComponent,
+    path: '',
+    canActivate: []
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
